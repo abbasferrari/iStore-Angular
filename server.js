af8a17cfb8ade8cb4ce541c5f562ab6c773ce58b
abbasferrari <abbas.ferrari@gmail.com>
2022-04-01 17:23:26 +0530

    Just using only cors without customization

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const app = express();
 /**/
 app.use(cors);
 // Serve static files....
-
+/*
 var corsOptions = {
     origin: 'https://lit-eyrie-15561.herokuapp.com/',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+}*/
 
 app.use(express.static(__dirname + '/dist/istore'));
 
 // Send all requests to index.html
-app.get('/*', cors(corsOptions), function(req, res) {
+app.get('/*'), function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/istore/index.html'));
 });
 
